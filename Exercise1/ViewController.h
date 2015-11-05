8002581f3cb50b0ffcdf6e10d3711589542e66d7
Yin Kokpheng <yinkokpheng@yins-mac-mini.local>
2015-11-05 14:30:27 +0700

    Initial Commit

---
+++ Exercise1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exercise1
+//
+//  Created by Yin Kokpheng on 11/5/15.
+//  Copyright © 2015 Yin Kokpheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
